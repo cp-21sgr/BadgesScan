bd71c6c3168a2b34e6046b36237a5307fbde97cf
NolanHiltbrand <nolan.hiltbrand@ceff.ch>
2023-06-02 10:18:15 +0200

    Graphic update

--- BadgesScan/Views/MainWindow.axaml.cs
+++ BadgesScan/Views/MainWindow.axaml.cs
@@ -30,8 +30,10 @@ namespace BadgesScan.Views
             CultureInfo ci = new CultureInfo("en-GB");
             string date = DateTime.Now.ToString("dddd, dd MMMM yyyy", ci);
             Date.Content = char.ToUpper(date[0]) + date.Substring(1);
-            
-            TimeElapsed.IsVisible = false;
+
+
+            ScanSuccess.IsVisible = false;
+            GreenScreen.IsVisible = false;
 
             client = new HttpClient();
         }
@@ -39,32 +41,34 @@ namespace BadgesScan.Views
         public async void ScanSimulation_Click(object sender, RoutedEventArgs e)
         {
             //do a scan
+            ScanSuccess.IsVisible = true;
+            GreenScreen.IsVisible = true;
             ScanSimulation.IsEnabled = false;
             if (isOddScan)
             {
-                TimeElapsed.IsVisible = false;
                 newOddScan = DateTime.Now;
                 ScanTime.Content = "Scan Time : " + newOddScan.ToString("HH:mm:ss");
 
-                await client.PostAsJsonAsync(apiUri, newOddScan);
+                //await client.PostAsJsonAsync(apiUri, newOddScan);
 
-                //await Task.Delay(2500);
+                await Task.Delay(2500);
 
             } else
             {
-                TimeElapsed.IsVisible = true;
                 DateTime newEvenScan = DateTime.Now;
                 ScanTime.Content = "Scan Time : " + newEvenScan.ToString("HH:mm:ss");
                 TimeSpan timeElapsed = newEvenScan - newOddScan;
                 TimeElapsed.Content = "Time Elapsed : " + $"{timeElapsed:hh\\:mm\\:ss}";
 
-                await client.PostAsJsonAsync(apiUri, newEvenScan);
+                //await client.PostAsJsonAsync(apiUri, newEvenScan);
 
-                //await Task.Delay(4000);
+                await Task.Delay(2500);
             }
             isOddScan = !isOddScan;
             ScanTime.Content = "Time of the scan";
-            TimeElapsed.IsVisible = false;
+            TimeElapsed.Content = "Time Elapsed";
+            ScanSuccess.IsVisible = false;
+            GreenScreen.IsVisible = false;
             ScanSimulation.IsEnabled = true;
         }
     }
